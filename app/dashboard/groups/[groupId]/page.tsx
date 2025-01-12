d5f73b8532c6bb38b4b5dcdbb890554d3c0c0003
Mariano Piva <mpiva24@gmail.com>
2025-01-12 00:01:23 -0300

    Fix failure deployment

--- app/dashboard/groups/[groupId]/page.tsx
+++ app/dashboard/groups/[groupId]/page.tsx
@@ -5,7 +5,8 @@ import styles from "./page.module.css";
 type Params = { groupId: string };
 
 export default async function GroupPage({ params }: { params: Params }) {
-  const groupId = await params.groupId;
+  const params1 = await params;
+  const groupId = await params1.groupId;
   const supabase = await createClient();
 
   try {
